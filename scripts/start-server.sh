4561a31af59ec806b7888628bc75f6cf40b9cd91
ich777 <christophhummer@gmail.com>
2023-10-18 17:09:11 +0200

    scripts/start-server.sh aktualisiert

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,55 +2,23 @@
 export DISPLAY=:99
 export XAUTHORITY=${DATA_DIR}/.Xauthority
 
-LAT_V="$(wget -qO- https://api.github.com/repos/TheTorProject/gettorbrowser/releases | jq -r '.[].tag_name' | grep "linux64-" | cut -d '-' -f2)"
-CUR_V="$(cat ${DATA_DIR}/application.ini 2>/dev/null | grep -E "^Version=[0-9].*" | cut -d '=' -f2)"
-if [ -z "$CUR_V" ]; then
-	if [ "${TOR_V}" != "latest" ]; then
-		LAT_V="$TOR_V"
-	fi
-else
-	if [ "${TOR_V}" == "latest" ]; then
-		LAT_V="$CUR_V"
-		if [ -z "$LAT_V" ]; then
-			echo "Something went horribly wrong with version detection, putting container into sleep mode..."
-			sleep infinity
-		fi
-	else
-		LAT_V="$TOR_V"
-	fi
-fi
+CUR_V=$(${DATA_DIR}/firefox --version 2>/dev/null | grep -E "^[0-9]*")
 
 rm ${DATA_DIR}/Tor-Browser-*.tar.xz 2>/dev/null
 
 if [ -z "$CUR_V" ]; then
-	echo "---Tor-Browser not installed, installing---"
-	cd ${DATA_DIR}
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz "https://github.com/TheTorProject/gettorbrowser/releases/download/linux64-${LAT_V}/tor-browser-linux64-${LAT_V}_ALL.tar.xz" ; then
-		echo "---Sucessfully downloaded Tor-Browser---"
-	else
-		echo "---Something went wrong, can't download Tor-Browser, putting container in sleep mode---"
-		rm -f ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz
-		sleep infinity
-	fi
-	tar -C ${DATA_DIR} --strip-components=2 -xf ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz
-	rm -f ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz
-elif [ "$CUR_V" != "$LAT_V" ]; then
-	echo "---Version missmatch, installed v$CUR_V, downloading and installing latest v$LAT_V...---"
-    cd ${DATA_DIR}
-	mkdir -p /tmp/profile
-	cp -R ${DATA_DIR}/TorBrowser/Data/Browser /tmp/profile/
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz "https://github.com/TheTorProject/gettorbrowser/releases/download/linux64-${LAT_V}/tor-browser-linux64-${LAT_V}_ALL.tar.xz" ; then
-		echo "---Sucessfully downloaded Tor-Browser---"
-	else
-		echo "---Something went wrong, can't download Tor-Browser, putting container in sleep mode---"
-		rm -f ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz
-		sleep infinity
-	fi
-	tar -C ${DATA_DIR} --strip-components=2 -xf ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz
-	rm -rf ${DATA_DIR}/TorBrowser/Data/Browser
-	cp -R /tmp/profile/Browser ${DATA_DIR}/TorBrowser/Data/
-	rm -f ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz
-	rm -rf /tmp/profile
+  LAT_V="$(wget -qO- https://api.github.com/repos/TheTorProject/gettorbrowser/releases | jq -r '.[].tag_name' | grep "linux64-" | cut -d '-' -f2)"
+  echo "---Tor-Browser not installed, installing---"
+  cd ${DATA_DIR}
+  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz "https://github.com/TheTorProject/gettorbrowser/releases/download/linux64-${LAT_V}/tor-browser-linux64-${LAT_V}_ALL.tar.xz" ; then
+    echo "---Sucessfully downloaded Tor-Browser---"
+  else
+    echo "---Something went wrong, can't download Tor-Browser, putting container in sleep mode---"
+    rm -f ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz
+    sleep infinity
+  fi
+  tar -C ${DATA_DIR} --strip-components=2 -xf ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz
+  rm -f ${DATA_DIR}/Tor-Browser-${LAT_V}.tar.xz
 fi
 
 echo "---Preparing Server---"
@@ -59,16 +27,16 @@ if [ -z "${CUSTOM_RES_W} ]; then
 	CUSTOM_RES_W=1024
 fi
 if [ -z "${CUSTOM_RES_H} ]; then
-	CUSTOM_RES_H=768
+  CUSTOM_RES_H=768
 fi
 
 if [ "${CUSTOM_RES_W}" -le 1023 ]; then
-	echo "---Width to low must be a minimal of 1024 pixels, correcting to 1024...---"
-    CUSTOM_RES_W=1024
+  echo "---Width to low must be a minimal of 1024 pixels, correcting to 1024...---"
+  CUSTOM_RES_W=1024
 fi
 if [ "${CUSTOM_RES_H}" -le 767 ]; then
-	echo "---Height to low must be a minimal of 768 pixels, correcting to 768...---"
-    CUSTOM_RES_H=768
+  echo "---Height to low must be a minimal of 768 pixels, correcting to 768...---"
+  CUSTOM_RES_H=768
 fi
 echo "---Checking for old logfiles---"
 find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
@@ -79,7 +47,7 @@ rm -rf /tmp/.X11*
 rm -rf ${DATA_DIR}/.vnc/*.log ${DATA_DIR}/.vnc/*.pid
 chmod -R ${DATA_PERM} ${DATA_DIR}
 if [ -f ${DATA_DIR}/.vnc/passwd ]; then
-	chmod 600 ${DATA_DIR}/.vnc/passwd
+  chmod 600 ${DATA_DIR}/.vnc/passwd
 fi
 screen -wipe 2&>/dev/null
 
